9cf96c7debb63d4851d1277bc3d760a1a74924f2
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-26 03:47:24 -0400

    scaffold app

---
+++ videos/src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component{
+    render(){
+        return <SearchBar />
+    }
+}
+export default App;
---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class SearchBar extends React.Component{
+    render(){
+        return <>APP</>
+    }
+}
+
+export default SearchBar;
