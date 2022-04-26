3d462eeb4e47c9171b872f4197c6428117323bd4
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-26 11:40:34 -0400

    adding parameters to my api

--- videos/src/APIS/youtube.js
+++ videos/src/APIS/youtube.js
@@ -3,5 +3,9 @@ import axios from 'axios'
 const KEY = 'AIzaSyBO0ArTUdnd8mhv2aAJACS2X-P-bsp3p-w';
 
 export default axios.create({
-
+    baseURL: 'https://www.googleapis.com/youtube/v3/videos',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+    }
 });
