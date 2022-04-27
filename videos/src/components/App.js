f7c0e203421322ffabe5606c31afc977fb58a921
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-26 20:36:22 -0400

    add css file

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../APIS/youtube";
-
+import VideoList from "./VideoList";
 
 class App extends React.Component{
 
@@ -22,6 +22,7 @@ class App extends React.Component{
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit}/>
             i have {this.state.videos.length} videos
+            <VideoList videos={this.state.videos}/>
         </div>
         )
     }
