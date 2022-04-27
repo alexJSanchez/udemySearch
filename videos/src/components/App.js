cc59d65f777d6f9e34ab6cba69e2da30c41716e8
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-26 22:36:25 -0400

    add new video

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,37 +4,48 @@ import youtube from "../APIS/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
-class App extends React.Component{
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
 
-    state = {
-        videos: [],
-        selectedVideo: null,
-    }
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items, 
+        selectedVideo: response.data.items[0]
+     });
+  };
 
-    onTermSubmit = async term => {
-     const response = await youtube.get('/search',{
-            params: {
-                q: term
-            }
-        })
-        this.setState({videos: response.data.items})
-    };
+  onVideoSelect = (video) => {
+    console.log("from the app", video);
+    this.setState({ selectedVideo: video });
+  };
 
-    onVideoSelect = (video) => {
-        console.log('from the app',video)
-        this.setState({selectedVideo: video})
-    }
-
-    render(){
-        return (
-        <div className="ui container">
-            <SearchBar onFormSubmit={this.onTermSubmit}/>
-            i have {this.state.videos.length} videos
-            <VideoDetail video={this.state.selectedVideo}/>
-            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />i have{" "}
+        {this.state.videos.length} videos
+        <div className="ui grid">
+          <div className="ui row">
+              <div className="ten wide column">
+            <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="six wide column">
+            <VideoList
+              onVideoSelect={this.onVideoSelect}
+              videos={this.state.videos}
+            />
+            </div>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 export default App;
-
