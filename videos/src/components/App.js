a45c7eee2f98ee04491241cdd9c84218fcf21874
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-26 21:08:16 -0400

    conditional video detail

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../APIS/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component{
 
     state = {
-        videos: []
+        videos: [],
+        selectedVideo: null,
     }
 
     onTermSubmit = async term => {
@@ -17,12 +19,19 @@ class App extends React.Component{
         })
         this.setState({videos: response.data.items})
     };
+
+    onVideoSelect = (video) => {
+        console.log('from the app',video)
+        this.setState({selectedVideo: video})
+    }
+
     render(){
         return (
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTermSubmit}/>
             i have {this.state.videos.length} videos
-            <VideoList videos={this.state.videos}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
         </div>
         )
     }
