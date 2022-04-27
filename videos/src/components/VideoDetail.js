68993b8d8a1801c728bc07413f3e0a89c08332d9
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-04-26 22:23:26 -0400

    add key

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -4,7 +4,20 @@ const VideoDetail = ({ video }) =>{
     if (!video){
         return <div>loading...</div>;
     }
-    return <div>{video.snippet.title}</div>;
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
+    return (
+    <div>
+        <div className="ui embed">
+            <iframe title="video player" src={videoSrc}/>
+        </div>
+        <div className="ui segment">
+            <h4 className="ui header">{video.snippet.title}</h4>
+            <p>{video.snippet.description}</p>
+        </div>
+    </div>
+    );
 }
 
 export default VideoDetail;
--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -4,7 +4,7 @@ import './VideoItem.css';
 const VideoItem = ({ video , onVideoSelect }) => {
   return (
     <div onClick={e => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
